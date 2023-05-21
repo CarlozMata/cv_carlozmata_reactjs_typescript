d9de54ec2e0d92f5b2e31b4a7b6593a0bd95f703
Carlos Alberto López Mata <carlos_1992_@hotmail.com>
2023-05-20 22:02:11 -0600

    update langs

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, List } from "antd";
+import { Avatar, Card, List, Space } from "antd";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import "../styles/cv.scss";
